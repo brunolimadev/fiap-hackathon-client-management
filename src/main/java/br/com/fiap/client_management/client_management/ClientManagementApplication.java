5abb67f957ab0a59c97eebc34cf074d51358f3af
Eric Rangel <b.ericrangel@hotmail.com>
2024-08-01 08:40:44 -0300

    Iniciando projeto

---
+++ src/main/java/br/com/fiap/client_management/client_management/ClientManagementApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.client_management.client_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/client_management/client_management/ClientManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.client_management.client_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
