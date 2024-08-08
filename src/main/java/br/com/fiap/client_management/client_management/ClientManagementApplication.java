8c2039815b1e6f06068920b4ed6338fd4774e6cc
Bruno Lima <bruno.lima.dev@outlook.com>
2024-08-07 22:55:49 -0300

    tests: improve coverage

--- src/main/java/br/com/fiap/client_management/client_management/ClientManagementApplication.java
+++ src/main/java/br/com/fiap/client_management/client_management/ClientManagementApplication.java
@@ -3,9 +3,12 @@ package br.com.fiap.client_management.client_management;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.Generated;
+
 @SpringBootApplication
 public class ClientManagementApplication {
 
+	@Generated
 	public static void main(String[] args) {
 		SpringApplication.run(ClientManagementApplication.class, args);
 	}
--- src/test/java/br/com/fiap/client_management/client_management/ClientManagementApplicationTests.java
+++ src/test/java/br/com/fiap/client_management/client_management/ClientManagementApplicationTests.java
@@ -1,10 +1,13 @@
 package br.com.fiap.client_management.client_management;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase
 class ClientManagementApplicationTests {
 
 	@Test
